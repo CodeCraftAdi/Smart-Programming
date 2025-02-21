0f9bb4a480b32374d2f968dc8fdb232f182aebb5
Aditya Jagtap <90320739+CodeCraftAdi@users.noreply.github.com>
2025-02-21 20:52:11 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,7 @@
+//version 2 
+public class Demo{
+
+    public static void main(String[] args) {
+        System.out.println("This is Demo Class2");
+    }
+}
---
+++ Test.java
@@ -0,0 +1,7 @@
+//verson 2
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("This is Test Class2");
+    }
+}
